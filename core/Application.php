ebe36294d4dfb5216d74f3b6bc4d26954c60a2cd
Marino <marino.pusic.5@gmail.com>
2024-07-07 23:57:11 +0200

    routes,application,views...

---
+++ core/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\core;
+class Application
+{
+    public static string $ROOT_DIR;
+    public Router $router;
+    public Request $request;
+
+    /**
+     * @param Router $router
+     */
+    public function __construct($rootPath)
+    {
+        self::$ROOT_DIR = $rootPath;
+        $this->request = new Request();
+        $this->router = new Router($this->request);
+
+    }
+    public function run(){
+        echo $this->router->resolve();
+    }
+
+
+}
---
+++ core/Router.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    protected array $routes = [];
+    public Request $request;
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
+    public function get($path,$callback){
+        $this->routes['get'][$path] = $callback;
+
+    }
+
+    public function resolve(){
+        $path =$this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if($callback === false){
+            return 'not found';
+        }
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback); // poziva funkciju 'Contact' na osnovu stringa koji je funkcijs
+
+    }
+    public function renderView($view){
+        $viewContent = $this->renderOnlyView($view);
+        $layoutContent = $this->layoutContent();
+        return str_replace('{{conntent}}',$viewContent,$layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start(); // start buff
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean(); // catch content
+
+
+    }
+    protected function renderOnlyView($view){
+        ob_start(); // start buff
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean(); // catch content
+
+    }
+
+
+}
