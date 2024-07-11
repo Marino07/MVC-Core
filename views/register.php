e726feac7aab243424d3998a8cfc44d018de97f4
Marino <marino.pusic.5@gmail.com>
2024-07-11 20:24:24 +0200

    form,fields auto upload

--- views/register.php
+++ views/register.php
@@ -1,8 +1,17 @@
-<?php
-?>
-<h1>Create account</h1>
 
-<form action="" method="post">
+<h1>Create account</h1>
+<?php $form =  \app\core\form\Form::begin('',"post"); ?>
+<?php echo $form->field($model,'firstname') ?>
+<?php echo $form->field($model,'lastname') ?>
+<?php echo $form->field($model,'email') ?>
+<?php echo $form->field($model,'password')->passwordField()?>
+<?php echo $form->field($model,'confirmPassword') ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+
+
+<?php echo \app\core\form\Form::end() ?>
+
+<!--<form action="" method="post">
     <div class="form-group">
         <label>First Name</label>
         <input type="text" name="firstname" class="form-control" >
@@ -24,4 +33,4 @@
         <input type="password" name="confirmPassword" class="form-control" >
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</form> <!-->
